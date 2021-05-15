e4196ffdceac8a7580c91920d0d82d94641260b7
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-15 15:35:25 +0200

    init bike repo

---
+++ MaturitaASP/Models/Bikes/BikeRepository.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MaturitaASP.Models.Bikes
+{
+    public class BikeRepository
+    {
+        private MyContext _database = new MyContext();
+
+        /*
+         * INSERT INTO DBO.DATABASE.TABLE VALUES ("HONDA", "130")
+         *
+         * public void Insert(Bike bike)
+         * {
+         *      this._database.Bike.Add(bike);
+         * }
+         */
+    }
+}
