57d19094fc77f197828a667532ab3a43f33203c6
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-15 16:55:14 +0200

    commands

--- MaturitaASP/Models/Bikes/BikeRepository.cs
+++ MaturitaASP/Models/Bikes/BikeRepository.cs
@@ -1,17 +1,22 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Linq;
+
 namespace MaturitaASP.Models.Bikes
 {
     public class BikeRepository
     {
         private MyContext _database = new MyContext();
 
-        /*
-         * INSERT INTO DBO.DATABASE.TABLE VALUES ("HONDA", "130")
-         *
-         * public void Insert(Bike bike)
-         * {
-         *      this._database.Bike.Add(bike);
-         * }
-         */
+        /*SELECT * FROM BIKE*/
+        public List<Bike> SelectAll()
+        {
+            return this._database.Bike.ToList();
+        }
+
+        public void Insert(Bike bike)
+        {
+            this._database.Bike.Add(bike);
+            this._database.SaveChanges();
+        }
     }
 }
