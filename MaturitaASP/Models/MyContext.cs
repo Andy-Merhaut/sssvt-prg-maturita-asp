0e22ec7d30c205730543606aec8f5b69d6ad9e94
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-15 15:23:36 +0200

    Přidejte soubory projektu.

---
+++ MaturitaASP/Models/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MaturitaASP.Models
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=yourdatabase;user=yourusername;password=yourpassword");
+        }
+    }
+}
