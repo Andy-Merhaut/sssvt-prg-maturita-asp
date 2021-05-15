2106bb27a616348a0e3fc5ed654d11ca1d8289de
Andy-Merhaut <andreasmerhaut11502@gmail.com>
2021-05-15 15:30:21 +0200

    bike class

--- MaturitaASP/Models/MyContext.cs
+++ MaturitaASP/Models/MyContext.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MaturitaASP.Models.Bikes;
+using Microsoft.EntityFrameworkCore;
 
 namespace MaturitaASP.Models
 {
     public class MyContext : DbContext
     {
-        public DbSet<> Person { get; set; }
+        public DbSet<Bike> Bike { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=yourdatabase;user=yourusername;password=yourpassword");
+            optionsBuilder.UseMySQL("server=localhost;database=bikes;user=root;password=");
         }
     }
 }
